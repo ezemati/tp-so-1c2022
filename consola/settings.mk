453cf73cdce85b48d53548e8c6e8bc48f0c28874
Ezequiel Saidman <esaidman@frba.utn.edu.ar>
2022-04-16 18:55:40 -0300

    Empiezo a migrar la estructura del proyecto a la de RaniAgus

---
+++ consola/settings.mk
@@ -0,0 +1,17 @@
+# Libraries
+LIBS=commons readline pthread cunit
+
+# Custom libraries' paths
+SHARED_LIBPATHS=
+STATIC_LIBPATHS=
+
+# Compiler flags
+CDEBUG=-g -Wall -DDEBUG
+CRELEASE=-O3 -Wall -DNDEBUG
+
+# Arguments when executing with start, memcheck or helgrind
+ARGS=cfg/programa.program 50
+
+# Valgrind flags
+MEMCHECK_FLAGS=--track-origins=yes --log-file="memcheck.log"
+HELGRIND_FLAGS=--log-file="helgrind.log"
