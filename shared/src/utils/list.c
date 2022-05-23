680affd9377874ca12b8edd6957376a49daff931
Ezequiel Saidman <esaidman@frba.utn.edu.ar>
2022-05-23 11:42:43 -0300

    Fix de posible memory leak

--- shared/src/utils/list.c
+++ shared/src/utils/list.c
@@ -2,13 +2,13 @@
 
 void *list_get_last_element(t_list *list)
 {
-    t_list_iterator *iterator = list_iterator_create(list);
-
     void *element = NULL;
+    t_list_iterator *iterator = list_iterator_create(list);
     while (list_iterator_has_next(iterator))
     {
         element = list_iterator_next(iterator);
     }
+    list_iterator_destroy(iterator);
 
     return element;
 }
