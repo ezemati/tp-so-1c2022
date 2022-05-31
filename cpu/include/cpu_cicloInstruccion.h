781064a8b44500af4a03811f9704316bcb266aa5
aufu <afuentes@est.frba.utn.edu.ar>
2022-05-31 12:10:40 -0300

    siguiendo

--- cpu/include/cpu_cicloInstruccion.h
+++ cpu/include/cpu_cicloInstruccion.h
@@ -7,30 +7,35 @@
 #include <stdlib.h>
 #include <time.h>
 
-typedef enum ciclo_proceso
+typedef enum ciclo_instruccion
 {
    FETCH,
    DECODE,
-   FETCHOPERANDS,
+   FETCH_OPERANDS,
    EXECUTE
-} ciclo_proceso;
+} ciclo_instruccion;
 
 typedef struct t_cpu_pcb
 {
    uint32_t id;
    uint32_t tamanio;
-   t_list *lista_instrucciones; // De tipo t_instruccion
-   ciclo_proceso estado;
+   t_list *lista_instrucciones; 
+   ciclo_instruccion estado;
    uint32_t program_counter;
    uint32_t tabla_paginas_primer_nivel;
    double estimacion_rafaga;
    uint32_t socket_consola;
-   uint32_t bloqueo_pendiente; // Indica la cantidad de milisegundos restantes que el proceso esta bloqueado
+   uint32_t bloqueo_pendiente; 
    time_t time_inicio_running;
    time_t time_fin_running;
-   double milisegundos_en_running; // En caso de que el proceso sea desalojado y vuelva a ejecutar, esto guarda el acumulado
+   double milisegundos_en_running; 
 } t_cpu_pcb;
 
-bool proxima_instruccion(t_list lista_instrucciones);
+
+void buscar_siguiente_instruccion(t_cpu_pcb *pcb);
+void interpretar_instruccion(t_cpu_pcb *pcb);
+void buscar_en_memoria_operandos(t_cpu_pcb *pcb);
+void ejecutar_instruccion(t_cpu_pcb *pcb);
+void check_interrupt(t_cpu_pcb *pcb); //VERIFICAR LO QUE TIENE QUE RECIBIR ES EL PCB
 
 #endif
